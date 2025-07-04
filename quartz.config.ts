66c8c4782a21e8e9763238c5391bfd4c6215fd63
Mark Comia <joobinsfromm@gmail.com>
2025-07-04 13:56:22 +0800

    Quartz sync: Jul 4, 2025, 1:56 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -56,7 +56,7 @@ const config: QuartzConfig = {
   plugins: {
     transformers: [
       Plugin.FrontMatter(),
-	  Plugin.HardLineBreas(),
+	  Plugin.HardLineBreaks(),
       Plugin.CreatedModifiedDate({
         priority: ["frontmatter", "git", "filesystem"],
       }),
